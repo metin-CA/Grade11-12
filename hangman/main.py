fb976ff4d854e8503040b1d908c79d94f383e564
metin <metin@gobindsarvar.ca>
2026-03-17 11:10:40 -0700

    hangman 2

--- hangman/main.py
+++ hangman/main.py
@@ -1,21 +1,86 @@
 import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["aardvark", "baboon", "camel"]
-
+lives = 6
 chosen_word = random.choice(word_list)
 
-display = ""
+correct_letters = []
 
 is_game_over = False
 while not is_game_over:
     guess = input("Make your guess: ").lower()
+    display = ""
 
+    if guess not in chosen_word:
+        lives -= 1
     for letter in chosen_word:
         if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
             display += letter
         else:
             display += "_"
 
+    if lives == 0:
+        is_game_over = True
+        print("You lose!")
+
+    print(stages[::-1][lives])
     print(display)
-
-
-# TODO-2: Change the for loop so that you keep the previous correct letters in display.
+    print("Lives: ", lives)
