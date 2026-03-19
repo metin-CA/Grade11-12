03d362cfac24cbe12da8a1f41148c1882d2c5379
metin <metin@gobindsarvar.ca>
2026-03-19 10:50:29 -0700

    hangman final

--- hangman/main.py
+++ hangman/main.py
@@ -1,25 +1,33 @@
 import random
 from hangman_words import word_list
 from hangman_art import stages, logo
+
 print(logo)
 lives = 6
 chosen_word = random.choice(word_list)
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
 
 correct_letters = []
 guessed_letters = []
 
 is_game_over = False
 while not is_game_over:
+    print(f"****************************{lives}/6 LIVES LEFT****************************")
     guess = input("Make your guess: ").lower()
-    display = ""
-
     if guess in guessed_letters:
         print(f"You have guessed {guess} before!")
-
     guessed_letters.append(guess)
+
+    display = ""
 
     if guess not in chosen_word:
         lives -= 1
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
     for letter in chosen_word:
         if letter == guess:
             display += letter
@@ -28,14 +36,14 @@ while not is_game_over:
             display += letter
         else:
             display += "_"
+    print("Word to guess: " + display)
 
     if lives == 0:
         is_game_over = True
-        print("You lose!")
+        print(f"***********************IT WAS {chosen_word}! YOU LOSE**********************")
+
+    if "_" not in display:
+        is_game_over = True
+        print("****************************YOU WIN****************************")
 
     print(stages[::-1][lives])
-    print(display)
-    print(f"################### You have {lives} left! ############################")
-
-# TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
-#  e.g. You guessed d, that's not in the word. You lose a life.
