73656f83b00c0aceaab5d4035022091b81f62739
metin <metin@gobindsarvar.ca>
2026-04-07 10:21:12 -0700

    final

--- calculator/main.py
+++ calculator/main.py
@@ -8,29 +8,34 @@ def multiply(num1, num2):
     return num1 * num2
 def divide(num1, num2):
     return num1 / num2
+def mod(num1, num2):
+    return num1 % num2
 operations = {
     "+": add,
     "-": subtract,
     "*": multiply,
-    "/": divide
+    "/": divide,
+    "%": mod
 }
 def calculator():
     print(logo)
     should_continue = True
+    num1 = float(input("What is the first number?: "))
+
     while should_continue:
-        num1 = int(input("Enter first number: "))
-        op = input("Pick an operation:\n + \n - \n * \n / \n ")
-        num2 = int(input("Enter the next number: "))
-        result = 0
-        for operation in operations:
-            if operation == op:
-                result = operations[operation](num1, num2)
-                print(f"Result: {num1} {operation} {num2} =  {result}")
-        should_stop = input(f"Type y to continue calculating with {result}, or type n to start a new calculation").lower()
-        if should_stop == "y":
-            should_continue = True
-            result += num2
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number?: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            num1 = answer
         else:
             should_continue = False
-
+            print("\n" * 20)
+            calculator()
 calculator()
